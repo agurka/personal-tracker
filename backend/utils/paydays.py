62b2a070ff262561eea22bc99f2c9e53d0818ef4
agurka <adrian.gurka@gmail.com>
2024-11-05 23:06:21 +0100

    remove print

--- backend/utils/paydays.py
+++ backend/utils/paydays.py
@@ -5,6 +5,5 @@ import os
 def get_path_to_data():
     config = utils.common.read_config()
     path_to_data = config["data_files"]["paydays"]
-    print(f"{path_to_data = }")
     assert os.path.exists(path_to_data)
     return path_to_data
