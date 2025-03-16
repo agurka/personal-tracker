90b77e1862be3d943fe10494ae9387da19068b0e
agurka <adrian.gurka@gmail.com>
2025-03-16 13:14:55 +0100

    fix broken path

--- utils/common.py
+++ utils/common.py
@@ -3,11 +3,11 @@ import json
 
 
 def read_config():
-    path = os.path.join(get_root_folder(), "backend", "config.json")
+    path = os.path.join(get_root_folder(), "config.json")
     with open(path, "r") as f:
         cfg = json.load(f)
     return cfg
 
 
 def get_root_folder():
-    return os.path.abspath(os.path.join(__file__, "..", "..", ".."))
+    return os.path.abspath(os.path.join(__file__, "..", ".."))
